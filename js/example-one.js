1744434d3710893a6fed4c22310344313cb6475a
dpwit <darren.whatford@dpwit.co.uk>
2021-03-28 16:52:33 +0100

    renamed to script.js, updated index.html and added first variable

--- js/example-one.js
+++
@@ -1,4 +0,0 @@
-let js = 'amazing'
-if (js === 'amazing') alert('JavaScript is fun!')
-
-console.log(40 + 8 + 23 - 10)
---
+++ js/script.js
@@ -0,0 +1,10 @@
+let js = 'amazing';
+
+console.log(40 + 8 + 23 - 10);
+
+//store values in variables, by declaring variables; variable is fistName, Darren is the value.
+let firstName = 'Darren';
+let lastName = 'Whatford';
+
+console.log(firstName);
+console.log(lastName);
