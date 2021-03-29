9e49ca1dd0ab85137eb051d21421f4443582c815
dpwit <darren.whatford@dpwit.co.uk>
2021-03-29 08:48:03 +0100

    Added boolean and undefined variable

--- js/assignments.js
+++ js/assignments.js
@@ -5,3 +5,9 @@ let population = 1000000;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+let isIsland = true;
+let language;
+
+console.log(isIsland);
+console.log(language);
