700e77908a49dd4c974b2d7fb020e7c890262b92
dpwit <darren.whatford@dpwit.co.uk>
2021-03-30 08:12:05 +0100

    added let and const variable examples

--- js/assignments.js
+++ js/assignments.js
@@ -1,3 +1,4 @@
+/*
 let country = "England";
 let continent = "Europe";
 let population = 1000000;
@@ -11,3 +12,12 @@ let language;
 
 console.log(isIsland);
 console.log(language);
+*/
+
+//let variables can change values
+let age = 30;
+age = 31;
+
+//const variables can't change and are constant and must have a value assigned.
+//use const by default
+const birthYear = 1969;
