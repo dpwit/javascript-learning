99f067fc9afb9d90cc5f146fbc9326f9f7135eea
dpwit <darren.whatford@dpwit.co.uk>
2021-03-30 21:00:49 +0100

    Split script and assignment js code

--- js/assignments.js
+++ js/assignments.js
@@ -1,35 +1,20 @@
-const country = "England";
-const continent = "Europe";
-let population = 1000000;
-
-console.log(country);
-console.log(continent);
-console.log(population);
-
+language = 'Englidh';
+const country = 'England';
+const continent = 'Europe';
+let population = 6;
 const isIsland = true;
-let language = "English";
 
-console.log(isIsland);
-console.log(language);
-
-
-//let variables can change values
-let age = 30;
-age = 31;
-
-//const variables can't change and are constant and must have a value assigned.
-//use const by default
-const birthYear = 1969;
-
-const currentYear = 2021;
-const ageDarren = currentYear - 1969;
-const ageJules = currentYear - 1970;
-const ageKate = currentYear - 2007;
-
-console.log(ageDarren, ageJules, ageKate);
-
-let x = 10 + 5;
-x += 10; // x = x + 10 = 25
-x *= 4; // x = x * 4 = 100
-x++; //x = x + 1
-console.log(x);
+console.log(population / 2);
+population++;
+console.log(population);
+console.log(population > 6);
+console.log(population < 33);
+const description =
+country +
+' is in ' +
+continent +
+', and its ' +
+population +
+' million people speak ' +
+language;
+console.log(description);
--- js/script.js
+++ js/script.js
@@ -1,10 +1,50 @@
-let js = 'amazing';
+/*
+const country = "England";
+const continent = "Europe";
+let population = 10;
+const description = country + ' is in ' + continent + ' and its population is ' + population + ' million ';
+console.log(country);
+console.log(continent);
+console.log(population);
+console.log(description);
 
-console.log(40 + 8 + 23 - 10);
+const isIsland = true;
+let language = "English";
 
-//store values in variables, by declaring variables; variable is fistName, Darren is the value.
-let firstName = 'Darren';
-let lastName = 'Whatford';
+console.log(isIsland);
+console.log(language);
 
-console.log(firstName);
-console.log(lastName);
+console.log(population / 2);
+population++;
+
+console.log(population > 6);
+console.log(population < 33);
+
+//let variables can change values
+let age = 30;
+age = 31;
+
+//const variables can't change and are constant and must have a value assigned.
+//use const by default
+const birthYear = 1969;
+
+const currentYear = 2021;
+const ageDarren = currentYear - 1969;
+const ageJules = currentYear - 1970;
+const ageKate = currentYear - 2007;
+
+console.log(ageDarren, ageJules, ageKate);
+
+let x = 10 + 5;
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; //x = x + 1
+console.log(x);
+*/
+
+const now = 2021;
+const ageDarren = now - 1969;
+const ageJules = now -1970;
+
+console.log(ageDarren > ageJules);
+console.log(now - 1969 >  now -1970);
