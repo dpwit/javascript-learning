1453f835cd9501e48560b808c6c7c2481bdca082
dpwit <darren.whatford@dpwit.co.uk>
2021-05-09 09:20:44 +0100

    added missing space

--- js/assignments.js
+++ js/assignments.js
@@ -10,5 +10,5 @@ console.log(population);
 console.log(population > 6);
 console.log(population < 33);
 
-const description = country + ' is in ' + continent + ', and its ' + population +' million people speak ' +language;
+const description = country + ' is in ' + continent + ', and its ' + population + ' million people speak ' + language;
 console.log(description);
